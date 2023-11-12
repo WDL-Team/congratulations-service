2101c2a0e58519328ec918b5f89047831c7f2aa4
mrHoft <ru.hoft@gmail.com>
2023-11-12 23:37:39 +0300

    feat: lower props usage

--- src/components/theme/ThemeSwitcher.tsx
+++ src/components/theme/ThemeSwitcher.tsx
@@ -4,34 +4,34 @@ import iconDark from '../../assets/crescent-moon.svg'
 import iconLight from '../../assets/sun-shape.svg'
 
 export const ThemeSwitcher = () => {
-  const { themeName, switchTheme } = useTheme()
+  const { switchTheme } = useTheme()
 
   return (
-    <StSwitchContainer onClick={switchTheme} name={themeName}>
+    <StSwitchContainer onClick={switchTheme}>
       <StIcon name="dark" />
       <StIcon name="light" />
-      <StSwitchToggle name={themeName} />
+      <StSwitchToggle />
     </StSwitchContainer>
   )
 }
 
-const StSwitchContainer = styled.div<{ name: string }>`
+const StSwitchContainer = styled.div`
   position: relative;
   display: inline-block;
   width: 48px;
   height: 24px;
-  background-color: ${props => (props.name === 'dark' ? props.theme.colors.tertiary : props.theme.colors.accent)};
+  background-color: ${({ theme }) => (theme.name === 'dark' ? theme.colors.tertiary : theme.colors.accent)};
   border-radius: 12px;
   cursor: pointer;
   transition: background-color 0.2s ease-in-out;
 `
-const StSwitchToggle = styled.div<{ name: string }>`
+const StSwitchToggle = styled.div`
   margin-top: 2px;
   width: 20px;
   height: 20px;
   background-color: ${props => props.theme.colors.secondary};
   border-radius: 50%;
-  transform: translateX(${props => (props.name === 'dark' ? '26px' : '2px')});
+  transform: translateX(${props => (props.theme.name === 'dark' ? '26px' : '2px')});
   transition: transform 0.2s ease-in-out;
 `
 const StIcon = styled.div<{ name: string }>`
@@ -41,6 +41,6 @@ const StIcon = styled.div<{ name: string }>`
   right: ${props => (props.name === 'light' ? '3px' : 'auto')};
   width: 18px;
   height: 18px;
-  background-image: url(${props => (props.name === 'dark' ? iconDark : iconLight)});
+  background-image: url(${props => (props.theme.name === 'dark' ? iconDark : iconLight)});
   background-size: cover;
 `
