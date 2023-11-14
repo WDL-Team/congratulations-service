984b387718d332690a79d1bf96ca3171ab8eaef3
Nikolay Hoft <51874769+mrHoft@users.noreply.github.com>
2023-11-14 11:26:23 +0300

    Feat/theme switcher (#24)
    * feat: theme switcher
    * feat: lower props usage

--- src/components/theme/ThemeProvider.tsx
+++ src/components/theme/ThemeProvider.tsx
@@ -7,10 +7,13 @@ import { ISettings } from '../../types/settings'
 const store = new Store()
 const settings = store.get('settings') as ISettings
 
+type ThemeVariantsFunc = (theme: ThemeVariants) => ThemeVariants
+
 interface ThemeContextProps {
   themeName: ThemeVariants
   theme: ITheme
-  setTheme: ((newTheme: ThemeVariants) => void) | null
+  setTheme: ((theme: ThemeVariants | ThemeVariantsFunc) => void) | (() => void)
+  switchTheme: () => void
 }
 
 export const ThemeContext = React.createContext<ThemeContextProps>({
@@ -18,16 +21,20 @@ export const ThemeContext = React.createContext<ThemeContextProps>({
   get theme(): ITheme {
     return ThemeArguments[this.themeName]
   },
-  setTheme: null,
+  setTheme: () => {},
+  switchTheme: () => {},
 })
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
   const [currentTheme, setCurrentTheme] = React.useState<ThemeVariants>((settings.theme ?? 'light') as ThemeVariants)
 
-  const setTheme = (newTheme: ThemeVariants) => {
+  const setTheme = (theme: ThemeVariants | ThemeVariantsFunc) => {
+    const newTheme = theme instanceof Function ? theme(currentTheme) : theme
     setCurrentTheme(newTheme)
     store.set('settings.theme', newTheme)
   }
+
+  const switchTheme = () => setTheme(prev => (prev === 'light' ? 'dark' : 'light'))
 
   return (
     <ThemeContext.Provider
@@ -35,6 +42,7 @@ export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
         themeName: currentTheme,
         theme: ThemeArguments[currentTheme],
         setTheme,
+        switchTheme,
       }}>
       <StThemeProvider theme={ThemeArguments[currentTheme]}>{children}</StThemeProvider>
     </ThemeContext.Provider>
