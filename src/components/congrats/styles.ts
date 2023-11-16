efb8ada5d45a0f348fe535edd022215bbc090eea
mrHoft <ru.hoft@gmail.com>
2023-11-16 21:32:22 +0300

    fix: preview styles

--- src/components/congrats/styles.ts
+++ src/components/congrats/styles.ts
@@ -1,11 +1,7 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const StContainer = styled.article<{ $preview: boolean }>`
-  box-sizing: border-box;
-  padding: 2rem;
-  width: ${props => (props.$preview ? '100%' : 'calc(100% - 4rem)')};
-  margin: ${props => (props.$preview ? '0' : '2rem')};
-  font-size: ${props => (props.$preview ? '12px' : '1rem')};
+const preview = css`
+  font-size: 12px;
   background-color: ${props => props.theme.colors.secondary};
   border-radius: 0.75rem;
   position: relative;
@@ -16,4 +12,12 @@ export const StContainer = styled.article<{ $preview: boolean }>`
     top: 0.5rem;
     right: 0.5rem;
   }
+`
+
+export const StContainer = styled.article<{ $preview: boolean }>`
+  box-sizing: border-box;
+  padding: 2rem;
+  width: ${props => (props.$preview ? '100%' : 'calc(100% - 4rem)')};
+  margin: ${props => (props.$preview ? '0' : '2rem')};
+  ${props => props.$preview && preview}
 `
