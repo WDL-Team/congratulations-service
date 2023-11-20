e5a81e3537a32a6cadde265cc0988dd938ee5d46
Nikolay Hoft <51874769+mrHoft@users.noreply.github.com>
2023-11-20 05:27:15 +0300

    feat: suspense indicator (#27)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import { SettingsProvider } from './components/context'
-import { Layout } from './components/layout'
-import { Constructor } from './components/constructor'
-import { Congrats } from './components/congrats'
-import { useRouter } from './hooks/useRouter'
+import React from 'react'
+import { useRouter } from '~/hooks/useRouter'
+import { SettingsProvider } from '@/context'
+import { Layout } from '@/layout'
+import { SuspenseIndicator } from '@/modal'
+import { Constructor } from '@/constructor'
+import { Congrats } from '@/congrats'
 
 function App() {
   const { query } = useRouter()
+  const [loading, setLoading] = React.useState(true)
+
+  React.useEffect(() => {
+    setLoading(!(document.readyState === 'interactive' || document.readyState === 'complete'))
+  }, [])
 
   return (
     <SettingsProvider>
       <Layout>{query ? <Congrats query={query} /> : <Constructor />}</Layout>
+      {loading && <SuspenseIndicator />}
     </SettingsProvider>
   )
 }
