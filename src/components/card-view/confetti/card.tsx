e87cc55112d24728c63242e86b1f83c89bd5bd7f
mrHoft <ru.hoft@gmail.com>
2023-11-26 10:10:51 +0300

    feat: new card views

---
+++ src/components/card-view/confetti/card.tsx
@@ -0,0 +1,45 @@
+import { useCallback } from 'react'
+import { StContainer, particles } from './styles'
+import type { TCardViewProps } from '..'
+import { parseText } from '../parse'
+import Particles from 'react-particles'
+import { loadConfettiPreset } from 'tsparticles-preset-confetti'
+import type { Engine } from 'tsparticles-engine'
+
+export function CardConfetti(props: TCardViewProps) {
+  const { name = '', text = '' } = props
+  const arr = parseText(text)
+
+  const particlesInit = useCallback(async (engine: Engine) => {
+    await loadConfettiPreset(engine)
+  }, [])
+
+  const options = {
+    fullScreen: false,
+    emitters: [
+      {
+        life: { duration: 5, count: 1 },
+        position: { x: 0, y: 30 },
+        particles: {
+          move: { direction: 'top-right' },
+        },
+      },
+      {
+        life: { duration: 5, count: 1 },
+        position: { x: 100, y: 30 },
+        particles: {
+          move: { direction: 'top-left' },
+        },
+      },
+    ],
+    preset: 'confetti',
+  }
+
+  return (
+    <StContainer>
+      <Particles style={particles} id="tsparticles" options={options} init={particlesInit} />
+      <h1 style={{ zIndex: 2 }}>{name}</h1>
+      <div style={{ zIndex: 2 }}>{arr}</div>
+    </StContainer>
+  )
+}
--- src/components/card-view/index.ts
+++ src/components/card-view/index.ts
@@ -2,6 +2,11 @@ import { CardBlank } from './blank'
 import { CardInky } from './inky'
 import { CardDotty } from './dotty'
 import { CardBubbles } from './bubbles'
+import { CardWholes } from './wholes'
+import { CardHexagon } from './hexagon'
+import { CardChars } from './chars'
+import { CardConfetti } from './confetti'
+import { CardSnow } from './snow'
 import type { TCongratsData } from '~/types/congrats'
 
 export type TCardViewProps = Omit<TCongratsData, 'card'>
@@ -10,7 +15,12 @@ enum CardView {
   Blank,
   Inky,
   Dotty,
+  Chars,
   Bubbles,
+  Wholes,
+  Hexagon,
+  Confetti,
+  Snow,
 }
 
 export const cardViewNames = Object.keys(CardView).filter(val => !Number.isFinite(Number(val)))
@@ -24,7 +34,12 @@ const cardViewList: TCardList = {
   [CardView.Blank]: CardBlank,
   [CardView.Inky]: CardInky,
   [CardView.Dotty]: CardDotty,
+  [CardView.Chars]: CardChars,
   [CardView.Bubbles]: CardBubbles,
+  [CardView.Wholes]: CardWholes,
+  [CardView.Hexagon]: CardHexagon,
+  [CardView.Confetti]: CardConfetti,
+  [CardView.Snow]: CardSnow,
 }
 
 export const getCardViewByName = (card: number | string = 0) => {
