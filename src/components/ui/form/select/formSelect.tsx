2b9851889986fe14fd27494105b60d24baaa013b
mrHoft <ru.hoft@gmail.com>
2023-12-03 19:39:46 +0300

    fix: lint

--- src/components/ui/form/select/formSelect.tsx
+++ src/components/ui/form/select/formSelect.tsx
@@ -5,7 +5,7 @@ import { StContainer, StSelect, StLabel, StSpan } from './styles'
 export const FormSelect = React.forwardRef((props: TFormSelectProps, ref: React.ForwardedRef<HTMLSelectElement>) => {
   const { id, options, placeholder, hidden, onChange, errorMessage, error } = props
   return (
-    <StContainer $hidden={hidden}>
+    <StContainer $hidden={!!hidden}>
       <StSelect id={id} onChange={onChange} ref={ref}>
         {options.map((opt, i) => (
           <option value={i} key={i}>
